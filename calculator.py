04e0f445064a9ceb7380b4ecda26b6c9496a1739
ItzOwoXD <71560713+ItzOwoXD@users.noreply.github.com>
2020-09-19 17:30:10 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+while True:
+    variable_name = int(input("Number 1: "))
+    variable_2 = int(input("Number 2: "))
+    print("Division= " + str(variable_name / variable_2))
+    print("Addition= " + str(variable_name + variable_2))
+    print("Subtraction= " + str(variable_name - variable_2))
+    print("Multiplying= " + str(variable_name * variable_2))
